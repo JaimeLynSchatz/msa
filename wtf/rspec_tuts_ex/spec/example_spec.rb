68e0daee3a16d0153a5b16612aafc652306739bd
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-05-06 10:27:46 -0700

    complete nettuts tutorial, tests pass

--- wtf/rspec_tuts_ex/spec/example_spec.rb
+++ wtf/rspec_tuts_ex/spec/example_spec.rb
@@ -1,9 +1,10 @@
-require 'spec_helper'
+require 'spec_helper'  # this isn't really working yet
 
 describe 'RSpecGreeter' do
   it 'RSpecGreeter#greet()' do
     greeter = RSpecGreeter.new          # Given
     greeting = greeter.greet            # When
     greeting.should eq('Hello RSpec!')  # Then
+ 
   end
 end
